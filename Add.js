5097da41282db79591f1c70d3327afaea3ddb834
wintage-sk <satheeshias212@gmail.com>
2023-08-10 15:12:01 +0530

    delete

--- Add.js
+++
@@ -1,5 +0,0 @@
-const form=document.querySelector('#form');
-const username=document.querySelector('#username');
-const email=document.querySelector('#email');
-const password=document.querySelector('#password');
-const cpassword=document.querySelector('#cpassword');
