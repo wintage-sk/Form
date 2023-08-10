fe48ed57e8dc2b51c80d08235d74a628d40e9675
wintage-sk <satheeshias212@gmail.com>
2023-08-10 15:10:18 +0530

    Add

---
+++ Add.js
@@ -0,0 +1,5 @@
+const form=document.querySelector('#form');
+const username=document.querySelector('#username');
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+const cpassword=document.querySelector('#cpassword');
